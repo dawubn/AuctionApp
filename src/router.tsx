490300c4a7069b5a8357e72d1197b08fbd3cb605
Dawid Urbaniak <dawid1745@gmail.com>
2026-01-29 22:19:40 +0100

    fix routers for github actions

--- src/router.tsx
+++ src/router.tsx
@@ -5,15 +5,20 @@ import { CreateAuction } from "./pages/CreateAuction";
 import { AuctionDetails } from "./pages/AuctionDetails";
 import { AuctionsList } from "./pages/AuctionList";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <Layout />,
+            children: [
+                { index: true, element: <Home /> },
+                { path: "auctions", element: <AuctionsList /> },
+                { path: "auctions/:id", element: <AuctionDetails /> },
+                { path: "create", element: <CreateAuction /> },
+            ],
+        },
+    ],
     {
-        path: "/",
-        element: <Layout />,
-        children: [
-            { index: true, element: <Home /> },
-            { path: "auctions", element: <AuctionsList /> },
-            { path: "auctions/:id", element: <AuctionDetails /> },
-            { path: "create", element: <CreateAuction /> },
-        ],
-    },
-]);
+        basename: import.meta.env.BASE_URL,
+    }
+);
