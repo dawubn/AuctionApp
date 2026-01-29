c26990d14744ad2ac55f32845afa8bf1c8f01d1e
Dawid Urbaniak <dawid1745@gmail.com>
2026-01-29 22:15:44 +0100

    vite config fix for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "AuctionApp",
+  base: "/AuctionApp/",
   plugins: [react(), tailwindcss()],
 })
